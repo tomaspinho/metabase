bc65796d6cf3371b98af3dd2d06b56efe280da9f
Alexander Polyankin <alexander.polyankin@metabase.com>
2021-09-27 19:46:12 +0300

    Unskip not broken tests

--- frontend/test/metabase/scenarios/sharing/alert/email_alert.cy.spec.js
+++ frontend/test/metabase/scenarios/sharing/alert/email_alert.cy.spec.js
@@ -1,4 +1,4 @@
-import { restore, setupLocalHostEmail } from "__support__/e2e/cypress";
+import { restore, setupDummySMTP } from "__support__/e2e/cypress";
 
 function setUpHourlyAlert(question_num) {
   cy.visit(`/question/${question_num}`);
@@ -9,8 +9,10 @@ function setUpHourlyAlert(question_num) {
 }
 
 describe("scenarios > alert > email_alert", () => {
-  beforeEach(restore);
-  beforeEach(cy.signInAsAdmin);
+  beforeEach(() => {
+    restore();
+    cy.signInAsAdmin();
+  });
 
   it("should have no alerts set up initially", () => {
     cy.server();
@@ -21,14 +23,10 @@ describe("scenarios > alert > email_alert", () => {
     });
   });
 
-  describe.skip("alert set up", () => {
-    // NOTE: To run tests, first run `python -m smtpd -n -c DebuggingServer localhost:1025` in your terminal
+  describe("alert set up", () => {
     beforeEach(() => {
       cy.server();
-
-      cy.visit("/admin/settings/email");
-      cy.findByText("SMTP Host");
-      setupLocalHostEmail();
+      setupDummySMTP();
     });
 
     it("should work with email alerts toggled on", () => {
@@ -47,24 +45,17 @@ describe("scenarios > alert > email_alert", () => {
       });
     });
 
-    it("should have email alerts toggled off (metabase#12349)", () => {
+    it.skip("should have email alerts toggled off (metabase#12349)", () => {
       // Turn off email alerts during alert setup
       setUpHourlyAlert(2);
+
       cy.findByText("Email")
         .parent()
         .find("a")
-        .click();
-      cy.findByText("Done")
         .click()
-        .then(() => {
-          cy.findAllByText("Orders");
-        });
+        .click();
 
-      // Check alert api is NOT sending email
-      cy.request("/api/alert").then(response => {
-        expect(response.body[0].channels).to.have.length(1);
-        expect(response.body[0].channels[0].recipients).to.equal("null");
-      });
+      cy.findByText("Done").should("be.disabled");
     });
   });
 });
