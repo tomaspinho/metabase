b9e843c29a34265551cdc053aef22f872b8b9bbd
Alexander Polyankin <alexander.polyankin@metabase.com>
2021-09-27 19:46:36 +0300

    Unskip not broken tests

--- frontend/test/metabase/scenarios/sharing/alert/email_alert.cy.spec.js
+++ frontend/test/metabase/scenarios/sharing/alert/email_alert.cy.spec.js
@@ -1,4 +1,4 @@
-import { restore, setupDummySMTP } from "__support__/e2e/cypress";
+import { restore, setupLocalHostEmail } from "__support__/e2e/cypress";
 
 function setUpHourlyAlert(question_num) {
   cy.visit(`/question/${question_num}`);
@@ -9,10 +9,8 @@ function setUpHourlyAlert(question_num) {
 }
 
 describe("scenarios > alert > email_alert", () => {
-  beforeEach(() => {
-    restore();
-    cy.signInAsAdmin();
-  });
+  beforeEach(restore);
+  beforeEach(cy.signInAsAdmin);
 
   it("should have no alerts set up initially", () => {
     cy.server();
@@ -23,10 +21,14 @@ describe("scenarios > alert > email_alert", () => {
     });
   });
 
-  describe("alert set up", () => {
+  describe.skip("alert set up", () => {
+    // NOTE: To run tests, first run `python -m smtpd -n -c DebuggingServer localhost:1025` in your terminal
     beforeEach(() => {
       cy.server();
-      setupDummySMTP();
+
+      cy.visit("/admin/settings/email");
+      cy.findByText("SMTP Host");
+      setupLocalHostEmail();
     });
 
     it("should work with email alerts toggled on", () => {
@@ -45,17 +47,24 @@ describe("scenarios > alert > email_alert", () => {
       });
     });
 
-    it.skip("should have email alerts toggled off (metabase#12349)", () => {
+    it("should have email alerts toggled off (metabase#12349)", () => {
       // Turn off email alerts during alert setup
       setUpHourlyAlert(2);
-
       cy.findByText("Email")
         .parent()
         .find("a")
-        .click()
         .click();
+      cy.findByText("Done")
+        .click()
+        .then(() => {
+          cy.findAllByText("Orders");
+        });
 
-      cy.findByText("Done").should("be.disabled");
+      // Check alert api is NOT sending email
+      cy.request("/api/alert").then(response => {
+        expect(response.body[0].channels).to.have.length(1);
+        expect(response.body[0].channels[0].recipients).to.equal("null");
+      });
     });
   });
 });
